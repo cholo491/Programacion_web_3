42ce836bd84cce1fec1550e5a6a598eed206dd30
cholo491 <saulsotosiles091@gmail.com>
2025-09-23 18:54:58 -0400

    OrderController creacion

---
+++ Aplicacion_de_Pedidios/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Aplicacion_de_Pedidios.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplicacion_de_Pedidios.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public OrderController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
