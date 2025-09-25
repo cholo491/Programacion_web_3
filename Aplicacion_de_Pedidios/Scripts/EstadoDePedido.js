9e828e910f0fa6680949f3061f5f317bb5fb45bf
cholo491 <saulsotosiles091@gmail.com>
2025-09-25 19:23:14 -0400

    Examen parcial uno agrefando model para Recipes

--- Aplicacion_de_Pedidios/Scripts/EstadoDePedido.js
+++ Aplicacion_de_Pedidios/Scripts/EstadoDePedido.js
@@ -1,1 +1,28 @@
-﻿
+﻿//Conseguir estado de pedido sql server
+function obtenerEstadoDePedido(pedidoId) {
+    pedidoId = pedidoId.trim();
+    if (pedidoId === "") {
+        alert("Error id del pedido no hallado");
+        return;
+    }
+    else {
+        fetch(`/api/pedidos/estado/${pedidoId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Error en la solicitud");
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data && data.estado) {
+                    document.getElementById("estadoPedido").innerText = `Estado del pedido ${pedidoId}: ${data.estado}`;
+                } else {
+                    document.getElementById("estadoPedido").innerText = "No se encontró el estado del pedido.";
+                }
+            })
+            .catch(error => {
+                console.error("Error al obtener el estado del pedido:", error);
+               +99999999066 document.getElementById("estadoPedido").innerText = "Error al obtener el estado del pedido.";
+            });
+    }
+}
--- Aplicacion_de_Pedidios/Scripts/TotalItemsAutomatico.js
+++ Aplicacion_de_Pedidios/Scripts/TotalItemsAutomatico.js
@@ -1,1 +1,9 @@
-﻿
+﻿//update
+function updateTotalItemsAutomatico() {
+    var totalItems = 0;
+    $('.item-row').each(function() {
+        var quantity = parseFloat($(this).find('.item-quantity').val()) || 0;
+        totalItems += quantity;
+    });
+    $('#total-items').text(totalItems);
+}
